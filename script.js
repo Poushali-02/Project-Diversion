4f6c9dfc91bd87e6c4f1f1fff9fdbc5922170c3f
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-02-09 22:03:48 +0530

    Project-Diversion new files added

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//Always js code event handling > inline event handling
-// One event can be handled once only through function but by event listener multiple access
+
 let navbar = document.querySelector(".nav-bar");
 const al = () => {
     alert("You are redirected to another page!");
 }
 navbar.addEventListener("click", al);//Event Listener  //Alerting on event -> click
-//-------------------------------------------------------------------------------------------
+
 //Dark mode Light mode
+let header = document.querySelector("#Heading");
 let toggle = document.querySelector("#modeToggle");
 let body = document.querySelector("body");
 let currMode = "light";
@@ -15,19 +15,21 @@ const mode = () =>{
         currMode = "dark";//changing value of currMode to dark
         toggle.innerText = "Light";//changing display
         body.style.backgroundColor = "black";
-        toggle.style.backgroundColor = "beige";
+        toggle.style.backgroundColor = "#EFE9D5";
         toggle.style.color = "black";
-        document.querySelector("body").style.color = "beige";
+        header.style.color = "#EFE9D5"
+        document.querySelector("body").style.color = "#EFE9D5";
         document.querySelector("body").style.borderColor = "black";//changing mode to dark
         
     } else{
         currMode = "light";//changing value of currMode to light
         toggle.innerText = "Dark";//changing display
-        body.style.backgroundColor = "beige";
+        body.style.backgroundColor = "#EFE9D5";
         toggle.style.backgroundColor = "black";
-        toggle.style.color = "beige";
+        toggle.style.color = "#EFE9D5";
+        header.style.color = "#27445D";
         document.querySelector("body").style.color = "black";
-        document.querySelector("body").style.borderColor = "beige"; //changing mode to light
+        document.querySelector("body").style.borderColor = "#EFE9D5"; //changing mode to light
     }
 }
 toggle.addEventListener("click", mode);
